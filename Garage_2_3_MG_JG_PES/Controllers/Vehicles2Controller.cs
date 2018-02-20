acfa6d703450fb37668e5db561aa1c64512f886e
Jan Gunnelin <jan.gunnelin@outlook.com>
2018-02-20 08:51:17 +0100

    Fixat dropdown för member och vehicletype

--- Garage_2_3_MG_JG_PES/Controllers/Vehicles2Controller.cs
+++ Garage_2_3_MG_JG_PES/Controllers/Vehicles2Controller.cs
@@ -80,7 +80,8 @@ namespace Garage_2_3_MG_JG_PES.Models
         // GET: Vehicles/Check-In
         public ActionResult CheckIn()
         {
-
+            ViewBag.MemberId = new SelectList(db.Members, "Id", "FirstName");
+            ViewBag.VehicleTypeID = new SelectList(db.VehicleTypes, "Id", "Type");
             return View();
         }
 
